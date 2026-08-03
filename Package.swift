961fac7ed14f32e7d47f28dde6c998b65aee2ab4
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-03 15:02:42 +0530

    Updated for release 0.15.0

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version:6.2.0
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "swift-parsing",
+    platforms: [
+        .iOS("13.0"),
+        .macOS("10.15"),
+        .tvOS("13.0"),
+        .watchOS("6.0")
+    ],
+    products: [
+        .library(
+            name: "Parsing",
+            targets: ["Parsing_Aggregation"]
+        )
+    ],
+    traits: [
+        .trait(
+            name: "CasePaths",
+            description: "Easily parse and print enums using CasePaths"
+        ),
+        .trait(
+            name: "default",
+            description: "The default traits of this package.",
+            enabledTraits: ["CasePaths"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/swift-precompiled/swift-case-paths",
+            revision: "1.9.1-precompiled.1"
+        )
+    ],
+    targets: [
+        .target(
+            name: "Parsing_Aggregation",
+            dependencies: [
+                "Parsing",
+                .product(
+                    name: "CasePaths",
+                    package: "swift-case-paths",
+                    condition: .when(traits: ["CasePaths"])
+                )
+            ]
+        ),
+        .binaryTarget(
+            name: "Parsing",
+            url: "https://github.com/swift-precompiled/swift-parsing/releases/download/0.15.0/Parsing-645821c009c32bef30f48c619281fcdb4b3181d08fa0053b19f4a34d741d06d6.xcframework.zip",
+            checksum: "645821c009c32bef30f48c619281fcdb4b3181d08fa0053b19f4a34d741d06d6"
+        )
+    ]
+)
